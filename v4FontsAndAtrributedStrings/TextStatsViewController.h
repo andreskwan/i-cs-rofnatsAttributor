920a0e832c3ae252eda3d26add782dc5731f3131
ANDRES KWAN <andreskwan@gmail.com>
2014-10-30 10:57:08 -0500

    v6 - Demo 1 - add a second MVC - test it independently

---
+++ v4FontsAndAtrributedStrings/TextStatsViewController.h
@@ -0,0 +1,15 @@
+ //
+//  TextStatsViewController.h
+//  v4FontsAndAtrributedStrings
+//
+//  Created by Andres Kwan on 10/27/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString * textToAnalyze;
+
+@end
---
+++ v4FontsAndAtrributedStrings/TextStatsViewController.m
@@ -0,0 +1,88 @@
+//
+//  TextStatsViewController.m
+//  v4FontsAndAtrributedStrings
+//
+//  Created by Andres Kwan on 10/27/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import "TextStatsViewController.h"
+
+@interface TextStatsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *colorfulCharactersLabel;
+@property (weak, nonatomic) IBOutlet UILabel *outlinedCharactersLabel;
+
+@end
+
+@implementation TextStatsViewController
+
+//I instantiate
+//the model
+//so viewWillAppear
+//could work with something
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.textToAnalyze = [[NSAttributedString alloc] initWithString:@"Kwancho"
+                                                         attributes:@{ NSForegroundColorAttributeName:[UIColor greenColor],
+                                                                    NSStrokeWidthAttributeName:@-3}];
+}
+
+-(void)setTextToAnalyze:(NSAttributedString *)textToAnalyze
+{
+    _textToAnalyze = textToAnalyze;
+    if (self.view.window) {
+        [self updateUI];
+    }
+    
+}
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self updateUI];
+}
+
+-(void)updateUI
+{
+    //1 identify chars with attribute XXX
+    //2 length of the string with characters that share the same attribute
+    //3 pass this lenght to the label
+    self.colorfulCharactersLabel.text =  [NSString stringWithFormat:@"%lu colorful characters", (unsigned long)[[self charactersWithAttribute:NSForegroundColorAttributeName] length] ];
+    self.outlinedCharactersLabel.text =  [NSString stringWithFormat:@"%lu outlined characters", (unsigned long)[[self charactersWithAttribute:NSStrokeWidthAttributeName] length] ];
+}
+
+-(NSAttributedString *)charactersWithAttribute:(NSString *) attributedName
+{
+    //local var to pollulled with data from the inspection of the
+    //specific attribute described by attributedName
+    NSMutableAttributedString * characters = [[NSMutableAttributedString alloc]init];
+    
+    //now analyze to identify the range of chars that chare the same attributedName
+
+    int index = 0;
+    while (index < [self.textToAnalyze length]) {
+        NSRange range;
+        id value = [self.textToAnalyze attribute:attributedName
+                                         atIndex:index
+                                  effectiveRange:&range];
+
+        //if value is nill didnt find any character with
+        //the specified attributedName
+        if (value) {
+            //append an attributed string to the characters
+            //attributed string
+            [characters appendAttributedString:
+             //extract the range of characters with the attribute
+             //specified.
+             [self.textToAnalyze attributedSubstringFromRange:range]];
+            //set the new index after the range
+            index = range.location + range.length;
+        } else {
+            //if not found at this index, go to the next.
+            index++;
+        }
+    }
+    return characters;
+}
+
+@end
